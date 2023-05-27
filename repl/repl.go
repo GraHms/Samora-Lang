9e4f11b2f7bd3060285bf4d18bb0a3db98a739cd
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2023-05-27 09:38:52 -0700

    chore: add read eval print loop

---
+++ repl/repl.go
@@ -0,0 +1,28 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"latin/lexer"
+	"latin/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			_, _ = fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
