1f6d6ef6d13ddaf9b646c703ea12e8d5a79ff685
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2023-06-10 05:44:08 -0700

    wip: bool abstract syntax tree

---
+++ ast/boolean.go
@@ -0,0 +1,12 @@
+package ast
+
+import "latin/token"
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
