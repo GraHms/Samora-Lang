87bf0d2d9884b71c51e3956a960ce2bed5ed4524
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2023-06-10 22:37:31 -0700

    feat: parse function literal

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -461,37 +461,38 @@ func TestIfElseExpression(t *testing.T) {
 	}
 }
 
-//func TestFunctionLiteralParsing(t *testing.T) {
-//	input := `fn(x, y) { x + y; }`
-//	l := lexer.New(input)
-//	program := l.ParseProgram()
-//	checkParserErrors(t, l)
-//	if len(program.Statements) != 1 {
-//		t.Fatalf("program.Statements does not contain 1 statements. got=%d", len(program.Statements))
-//
-//	}
-//	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
-//	if !ok {
-//		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
-//	}
-//	function, ok := stmt.Expression.(*ast.FunctionLiteral)
-//	if !ok {
-//		t.Fatalf("stmt.Expression is not ast.FunctionLiteral. got=%T", stmt.Expression)
-//	}
-//	if len(function.Parameters) != 2 {
-//		t.Fatalf("function literal parameters wrong. want 2, got=%d", len(function.Parameters))
-//	}
-//	testLiteralExpression(t, function.Parameters[0], "x")
-//	testLiteralExpression(t, function.Parameters[1], "y")
-//	if len(function.Body.Statements) != 1 {
-//		t.Fatalf("function.Body.Statements has not 1 statements. got=%d", len(function.Body.Statements))
-//	}
-//	bodyStmt, ok := function.Body.Statements[0].(*ast.ExpressionStatement)
-//	if !ok {
-//		t.Fatalf("function body stmt is not ast.ExpressionStatement. got=%T", function.Body.Statements[0])
-//	}
-//	testInfixExpression(t, bodyStmt.Expression, "x", "+", "y")
-//}
+func TestFunctionLiteralParsing(t *testing.T) {
+	input := `fn(x, y) { x + y; }`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 statements. got=%d", len(program.Statements))
+
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+	function, ok := stmt.Expression.(*ast.FunctionLiteral)
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.FunctionLiteral. got=%T", stmt.Expression)
+	}
+	if len(function.Parameters) != 2 {
+		t.Fatalf("function literal parameters wrong. want 2, got=%d", len(function.Parameters))
+	}
+	testLiteralExpression(t, function.Parameters[0], "x")
+	testLiteralExpression(t, function.Parameters[1], "y")
+	if len(function.Body.Statements) != 1 {
+		t.Fatalf("function.Body.Statements has not 1 statements. got=%d", len(function.Body.Statements))
+	}
+	bodyStmt, ok := function.Body.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("function body stmt is not ast.ExpressionStatement. got=%T", function.Body.Statements[0])
+	}
+	testInfixExpression(t, bodyStmt.Expression, "x", "+", "y")
+}
 
 func TestFunctionParameterParsing(t *testing.T) {
 	tests := []struct {
