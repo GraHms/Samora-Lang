c1fa815fe74415912bc4c083546a5ade8bd4257c
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2023-06-11 01:54:29 -0700

    wip(eval): evaluate integers with minus prefix

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -14,6 +14,8 @@ func TestEvaluateIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
