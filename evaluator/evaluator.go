5125188ea3a05428fc7af5bface304a23361c39a
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2023-06-11 01:33:25 -0700

    wip(eval): recursively evaluate the ast node

---
+++ evaluator/evaluator.go
@@ -0,0 +1,27 @@
+package evaluator
+
+import (
+	"latin/ast"
+	"latin/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+	return result
+}
