6acc64bf1dcfe0da1e8263eebbf02617ed410542
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2023-06-11 06:14:52 -0700

    chore: bind function, threat functions as first class citezens

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -18,6 +18,16 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.ExpressionStatement:
 
 		return Eval(node.Expression, env)
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpression(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+		return applyFunction(function, args)
 	case *ast.FunctionLiteral:
 		params := node.Parameters
 		body := node.Body
@@ -218,4 +228,40 @@ func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object
 		return val
 	}
 	return newError("identifier not found: " + node.Value)
+}
+
+func evalExpression(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
+}
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+	for i, param := range fn.Parameters {
+		env.Set(param.Value, args[i])
+	}
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+	return obj
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -215,3 +215,20 @@ func TestFunctionObject(t *testing.T) {
 		t.Fatalf("body is not %q. got=%q", expectedBody, fn.Body.String())
 	}
 }
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x){x;}; identity(5);", 5},
+		{"let identity = fn(x){return x;}; identity(5);", 5},
+		{"let double = fn(x){x*2;}; double(5);", 10},
+		{"let add = fn(x,y){x+y;}; add(5,5);", 10},
+		{"let add = fn(x,y){x+y;}; add(5+5,add(5,5));", 20},
+		{"fn(x){x;}(5)", 5},
+	}
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
