1ac6a44e4f674e5dd5031a0224ca0076ff2f5b9f
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2023-07-03 09:50:10 -0700

    ref(main): remove unused code

--- main.go
+++ main.go
@@ -11,23 +11,6 @@ import (
 	"os"
 )
 
-//import (
-//	"fmt"
-//	"latin/repl"
-//	"os"
-//	user2 "os/user"
-//)
-//
-//func main() {
-//	user, err := user2.Current()
-//	if err != nil {
-//		panic(err)
-//	}
-//	fmt.Printf("Hi %s! This is the Latin Program", user.Username)
-//	fmt.Printf("Feel free to type in commands\n")
-//	repl.Start(os.Stdin, os.Stdout)
-//}
-
 func main() {
 
 	flag.Parse()
